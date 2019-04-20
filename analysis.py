41b9eebd0b3ae43d6ecf75c70d45842ca15ac7f6
maoyuanli <maoyuan@gmail.com>
2019-04-20 18:20:03 -0400

    converts Java naming convention to PEP8 standards (analysis part)

--- analysis.py
+++ analysis.py
@@ -6,19 +6,20 @@ import string
 import heapq
 
 
-def processWords(raw, removePunc=False, addStopwords=False, stem=False):
+def process_words(raw, remove_punc=False, add_stopwords=False, stem=False):
     raw = str(raw).lower()
-    if removePunc:
+    if remove_punc:
         nopunc = [c for c in raw if c not in string.punctuation]
         raw = ''.join(nopunc)
-    stopwordsList = []
-    stopwordsList_en = set(stopwords.words('english'))
-    stopwordsList_fr = set(stopwords.words('french'))
-    stopwordsList.extend(stopwordsList_en)
-    stopwordsList.extend(stopwordsList_fr)
-    if addStopwords:
-        stopwordsList.extend(additionalStopwords())
-    nostop = [w for w in raw.split() if w.lower() not in stopwordsList]
+
+    stopwords_list = []
+    stopwords_list_en = set(stopwords.words('english'))
+    stopwords_list_fr = set(stopwords.words('french'))
+    stopwords_list.extend(stopwords_list_en)
+    stopwords_list.extend(stopwords_list_fr)
+    if add_stopwords:
+        stopwords_list.extend(additional_stopwords())
+    nostop = [w for w in raw.split() if w.lower() not in stopwords_list]
     if stem:
         stemmer = PorterStemmer()
         return ' '.join([stemmer.stem(t) for t in nostop])
@@ -26,12 +27,12 @@ def processWords(raw, removePunc=False, addStopwords=False, stem=False):
         return ' '.join(nostop)
 
 
-def additionalStopwords():
-    stopwordsFile = open('additional_stopwordsList.txt')
-    return [w.lower().replace('\n', '') for w in stopwordsFile.readlines()]
+def additional_stopwords():
+    stopwords_file = open('additional_stopwordsList.txt')
+    return [w.lower().replace('\n', '') for w in stopwords_file.readlines()]
 
 
-def wordWeight(s):
+def word_weight(s):
     fdist = nltk.FreqDist(s.split())
     fdict = dict(fdist)
     wdict = {}
@@ -40,22 +41,22 @@ def wordWeight(s):
     return wdict
 
 
-def sentenceScore(sentences, wordWeightDict: dict):
-    sentScore = {}
+def sentence_score(sentences, word_weight_dict: dict):
+    sent_score = {}
     for sentence in sentences:
         for word in nltk.word_tokenize(sentence):
-            if word in wordWeightDict.keys():
-                if sentence not in sentScore.keys():
-                    sentScore[sentence] = wordWeightDict[word]
+            if word in word_weight_dict.keys():
+                if sentence not in sent_score.keys():
+                    sent_score[sentence] = word_weight_dict[word]
                 else:
-                    sentScore[sentence] += wordWeightDict[word]
-    return sentScore
+                    sent_score[sentence] += word_weight_dict[word]
+    return sent_score
 
 
 df = pd.read_json('data.json')
 df.dropna(subset=['descr'], inplace=True)
-df['descr_no_punc'] = df['descr'].apply(lambda x: processWords(x, removePunc=True))
-df['descr_sent_token'] = df['descr'].apply(lambda x: nltk.sent_tokenize(processWords(x)))
-df['descr_word_wt'] = df['descr_no_punc'].apply(lambda x: wordWeight(x))
-df['descr_sent_score'] = df.apply(lambda df: sentenceScore(df['descr_sent_token'], df['descr_word_wt']), axis=1)
+df['descr_no_punc'] = df['descr'].apply(lambda x: process_words(x, remove_punc=True))
+df['descr_sent_token'] = df['descr'].apply(lambda x: nltk.sent_tokenize(process_words(x)))
+df['descr_word_wt'] = df['descr_no_punc'].apply(lambda x: word_weight(x))
+df['descr_sent_score'] = df.apply(lambda df: sentence_score(df['descr_sent_token'], df['descr_word_wt']), axis=1)
 df['descr_summ'] = df['descr_sent_score'].apply(lambda d: heapq.nlargest(3, d, key=d.get))
